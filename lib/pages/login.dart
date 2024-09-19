123953d5d0a83008bbb61b04f7f3e27e0bc348db
mateusscarin <mateus.scarin@gmail.com>
2024-09-19 20:16:53 -0300

    feat: change image from login view

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -17,7 +17,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text("LOGIN")),
+      appBar: AppBar(centerTitle: true, title: const Text("MAGAZINELUIZ")),
       backgroundColor:
           Colors.grey[200], // Altere a cor de fundo conforme necessário
       body: Padding(
@@ -27,47 +27,18 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             Container(
               decoration: BoxDecoration(
-                borderRadius:
-                    BorderRadius.circular(20.0), // Bordas arredondadas
                 image: DecorationImage(
-                  image: AssetImage('assets/cartoes.png'), // Caminho da imagem
+                  image:
+                      AssetImage('assets/login-image.png'), // Caminho da imagem
                   fit: BoxFit.cover,
                 ),
               ),
-              height: 100.0, // Ajuste o tamanho conforme necessário
+              height: 270.0, // Ajuste o tamanho conforme necessário
             ),
             const SizedBox(height: 15.0), // Espaço entre a imagem e o texto
             const Text(
               "Qual sua função?",
               style: TextStyle(fontSize: 24.0),
-            ),
-            const SizedBox(height: 15.0),
-            InkWell(
-              onTap: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) => const AdminHomePage(),
-                //   ),
-                // );
-              },
-              child: Container(
-                height: 50.0,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 14, 105, 170),
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 14, 105, 170),
-                  ),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: const Center(
-                  child: Text(
-                    "ADMIN",
-                    style: TextStyle(fontSize: 16.0, color: Colors.white),
-                  ),
-                ),
-              ),
             ),
             const SizedBox(height: 15.0),
             InkWell(
@@ -83,15 +54,16 @@ class _LoginPageState extends State<LoginPage> {
                 height: 50.0,
                 width: double.infinity,
                 decoration: BoxDecoration(
+                  color: const Color.fromRGBO(115, 187, 237, 1),
                   border: Border.all(
-                    color: const Color.fromARGB(255, 14, 105, 170),
+                    color: const Color.fromRGBO(115, 187, 237, 1),
                   ),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 child: const Center(
                   child: Text(
-                    "USER",
-                    style: TextStyle(fontSize: 16.0, color: Colors.black),
+                    "ADMIN",
+                    style: TextStyle(fontSize: 16.0, color: Color.fromARGB(255, 65, 65, 65)),
                   ),
                 ),
               ),
